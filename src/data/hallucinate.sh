ac0c8bb868484b1d4545d39b6d9fa0fbc93146ed
AssafSin <as12152@nyu.edu>
2020-05-29 15:57:48 -0400

    deleted libalign

--- src/data/hallucinate.sh
+++ src/data/hallucinate.sh
@@ -3,6 +3,6 @@
 mkdir -p task0-data/hall
 
 for lng in ceb dje gaa gmh gml hil izh kjh kon lin lud mao mlg mwf sot tel tgk vro xno zpv zul; do
-python hallucinate.py task0-data/out $lng --examples 10000
+python src/data/hallucinate.py task0-data/out $lng --examples 10000
 mv -v task0-data/out/$lng.hall task0-data/hall/$lng.hall
 done
